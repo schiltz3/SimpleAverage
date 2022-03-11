0892e6fe2b69e0a5b2ec614e180003009f86d128
John Schiltz <johnschiltz2000@gmail.com>
2022-03-11 01:23:08 -0600

    Added tests

---
+++ SimpleAverage/test/test_simpleaverage.c
@@ -0,0 +1,16 @@
+#include "SimpleAverage.h" 
+#include "../../Unity/src/unity.h" 
+ 
+ 
+void setUp(void) 
+{ 
+} 
+ 
+/** 
+ * @brief Tests passing null to init
+ * 
+ */ 
+void test_NullInit() 
+{ 
+} 
+ 

38fdd0cd1770a4df7e0eaa8b57375f2d2d01cf26
John Schiltz <johnschiltz2000@gmail.com>
2022-03-22 15:25:29 -0500

    Added one test to check proper init

--- SimpleAverage/test/test_simpleaverage.c
+++ SimpleAverage/test/test_simpleaverage.c
@@ -13,11 +13,18 @@ void setUp(void)
  * @brief Tests passing null to init
  * 
  */ 
-void test_Nullnit() 
+void test_NullInit() 
 { 
   int ret = InitSimpleAverage(NULL);
   TEST_ASSERT_EQUAL_INT_MESSAGE(-1, ret, "Did not return error (-1)");
 } 
+
+void test_Init() {
+  int ret = InitSimpleAverage(&test_simple_average);
+  TEST_ASSERT_EQUAL_INT_MESSAGE(0, ret, "Did not return correct init (0)");
+  TEST_ASSERT_EQUAL_INT_MESSAGE(0, test_simple_average.count, "Count should have been reset to 0");
+  TEST_ASSERT_EQUAL_INT_MESSAGE(0, test_simple_average.sum, "Sum should have been reset to 0");
+}
 
 void test_10() {
   test_simple_average.sum = 10;
